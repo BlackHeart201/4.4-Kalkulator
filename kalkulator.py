fd621ba12bb4d5c1ba941551b0039a7e7337e5a6
Piotr Polak <polak.piotr94@gmail.com>
2021-06-08 23:15:17 +0200

    Add logging 4.4-Kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,3 +1,7 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 operation = input(
     "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
 
@@ -5,23 +9,23 @@ first_number = float(input("Podaj pierwszą liczbę:"))
 second_number = float(input("Podaj drugą liczbę:"))
 if operation == "1":
     sum_result = first_number + second_number
-    print("Dodaję: ", first_number, " ", "i ", second_number)
+    logging.debug("Dodaję: ", first_number, " ", "i ", second_number)
     print("Wynik to: ", sum_result)
 
 elif operation == "2":
     result = first_number - second_number
-    print("Odejmuję: ", first_number, "i ", second_number)
+    logging.debug("Odejmuję: ", first_number, "i ", second_number)
     print("Wynik to: ", result)
 elif operation == "3":
     result = first_number * second_number
-    print("Mnożę: ", first_number, "i ", second_number)
+    logging.debug("Mnożę: ", first_number, "i ", second_number)
     print("Wynik to: ", result)
 elif operation == "4":
     if second_number == 0:
         print("Nie dziel przez zero!")
     else:
         result = first_number / second_number
-        print("Dzielę: ", first_number, "przez ", second_number)
+        logging.debug("Dzielę: ", first_number, "przez ", second_number)
         print("Wynik to: ", result)
 else:
     print("Brak funkcji dla podanej wartości")
