ed266e75110a43c90186440ca4065f4357448eee
BlackHeart201 <84109863+BlackHeart201@users.noreply.github.com>
2021-06-15 21:40:12 +0200

    Funkcje

--- kalkulator.py
+++ kalkulator.py
@@ -1,42 +1,61 @@
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
-
-def operation_list(x):
-    return {
-        "Dodawanie": 1,
-        "Odejmowanie": 2,
-        "Mnożenie": 3,
-        "Dzielenie": 4,
-    }[x]
-
-
 while True:
-    operation = input(
-        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+    operation = int(input(
+        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
+    first_number = int(input("Podaj pierwszą liczbę:"))
+    second_number = int(input("Podaj drugą liczbę:"))
 
-    first_number = float(input("Podaj pierwszą liczbę:"))
-    second_number = float(input("Podaj drugą liczbę:"))
-    if operation == "1":
-        sum_result = first_number + second_number
-        logging.debug("Dodaję: ", first_number, " ", "i ", second_number)
-        print("Wynik to: ", sum_result)
 
-    elif operation == "2":
-        result = first_number - second_number
-        logging.debug("Odejmuję: ", first_number, "i ", second_number)
-        print("Wynik to: ", result)
-    elif operation == "3":
-        result = first_number * second_number
-        logging.debug("Mnożę: ", first_number, "i ", second_number)
-        print("Wynik to: ", result)
-    elif operation == "4":
-        if second_number == 0:
-            print("Nie dziel przez zero!")
-        else:
-            result = first_number / second_number
-            logging.debug("Dzielę: ", first_number, "przez ", second_number)
-            print("Wynik to: ", result)
-    else:
-        print("Brak funkcji dla podanej wartości")
+    def addition(a, b):
+        return a + b
+
+
+    addition_function = addition(first_number, second_number)
+
+
+    def subtraction(a, b):
+        return a - b
+
+
+    subtraction_function = subtraction(first_number, second_number)
+
+
+    def division(a, b):
+        return a / b
+
+
+    division_function = division(first_number, second_number)
+
+
+    def multiplication(a, b):
+        return a * b
+
+
+    multiplication_function = multiplication(first_number, second_number)
+
+
+    def operation_list(x):
+        return {
+            1: [addition_function, "Dodawanie"],
+            2: [subtraction_function, "Odejmowanie"],
+            3: [division_function, "Mnożenie"],
+            4: [multiplication_function, "Dzielenie"],
+        }[x]
+
+    dictionary_description = operation_list(operation)
+
+    operation_result = dictionary_description[0]
+    operation_name = dictionary_description[1]
+
+    print(operation_result)
+    # print(operation_name)
+
+""" dict = {
+    1: ["a","b"],
+    2: ["c","d"]
+}
+
+first = dict.get(1)
+
+nazwa = first[1]
+
+print(nazwa) """
