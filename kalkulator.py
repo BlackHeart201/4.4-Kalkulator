db54e56e73aa2c451c2fcb5006eeeb98312f0430
blackheart <polak.piotr94@gmail.com>
2021-06-15 22:18:35 +0200

    Final asumptions

--- kalkulator.py
+++ kalkulator.py
@@ -19,13 +19,6 @@ while True:
     subtraction_function = subtraction(first_number, second_number)
 
 
-    def division(a, b):
-        return a / b
-
-
-    division_function = division(first_number, second_number)
-
-
     def multiplication(a, b):
         return a * b
 
@@ -33,29 +26,26 @@ while True:
     multiplication_function = multiplication(first_number, second_number)
 
 
+    def division(a, b):
+        return a / b
+
+
+    division_function = division(first_number, second_number)
+
+
     def operation_list(x):
         return {
-            1: [addition_function, "Dodawanie"],
-            2: [subtraction_function, "Odejmowanie"],
-            3: [division_function, "Mnożenie"],
-            4: [multiplication_function, "Dzielenie"],
+            1: [addition_function, "Dodaję: "],
+            2: [subtraction_function, "Odejmuję: "],
+            3: [multiplication_function, "Mnożę: "],
+            4: [division_function, "Dzielę: "],
         }[x]
+
 
     dictionary_description = operation_list(operation)
 
     operation_result = dictionary_description[0]
     operation_name = dictionary_description[1]
 
-    print(operation_result)
-    # print(operation_name)
-
-""" dict = {
-    1: ["a","b"],
-    2: ["c","d"]
-}
-
-first = dict.get(1)
-
-nazwa = first[1]
-
-print(nazwa) """
+    print(operation_name, first_number, " i ", second_number)
+    print("Wynik to: ", operation_result)
