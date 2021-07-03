3b1294ebce99ffa7028c3613dbe369b57e55b3b8
BlackHeart201 <84109863+BlackHeart201@users.noreply.github.com>
2021-07-03 14:23:09 +0200

    Poprawa referencji funkcji

--- kalkulator.py
+++ kalkulator.py
@@ -1,3 +1,24 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def addition(a, b):
+    return a + b
+
+
+def subtraction(a, b):
+    return a - b
+
+
+def division(a, b):
+    return a / b
+
+
+def multiplication(a, b):
+    return a * b
+
+
 while True:
     operation = int(input(
         "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
@@ -5,57 +26,19 @@ while True:
     second_number = int(input("Podaj drugą liczbę:"))
 
 
-    def addition(a, b):
-        return a + b
-
-
-    addition_function = addition(first_number, second_number)
-
-
-    def subtraction(a, b):
-        return a - b
-
-
-    subtraction_function = subtraction(first_number, second_number)
-
-
-    def division(a, b):
-        return a / b
-
-
-    division_function = division(first_number, second_number)
-
-
-    def multiplication(a, b):
-        return a * b
-
-
-    multiplication_function = multiplication(first_number, second_number)
-
-
     def operation_list(x):
         return {
-            1: [addition_function, "Dodawanie"],
-            2: [subtraction_function, "Odejmowanie"],
-            3: [division_function, "Mnożenie"],
-            4: [multiplication_function, "Dzielenie"],
+            1: [addition(first_number, second_number), "Dodawanie", "Dodaję"],
+            2: [subtraction(first_number, second_number), "Odejmowanie", "Odejmuję"],
+            3: [multiplication(first_number, second_number), "Mnożenie", "Mnożę"],
+            4: [division(first_number, second_number), "Dzielenie", "Dzielę"],
         }[x]
 
-    dictionary_description = operation_list(operation)
 
+    dictionary_description = operation_list(operation)
     operation_result = dictionary_description[0]
     operation_name = dictionary_description[1]
+    operation_function = dictionary_description[2]
 
-    print(operation_result)
-    # print(operation_name)
-
-""" dict = {
-    1: ["a","b"],
-    2: ["c","d"]
-}
-
-first = dict.get(1)
-
-nazwa = first[1]
-
-print(nazwa) """
+    print("{}: {} i {}".format(operation_function, first_number, second_number))
+    print("Wynik to: {} ".format(operation_result))
